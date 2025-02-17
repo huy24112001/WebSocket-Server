0995816b242e0e365bb1170ae575451b458f11c4
assassin_24 <huyassassin24@gmail.com>
2025-02-17 23:26:03 +0700

    update address server 1

--- serverWebSocket.js
+++ serverWebSocket.js
@@ -1,8 +1,9 @@
+require('dotenv').config();
 const { createServer } = require("http");
 const { Server } = require("socket.io");
 const crypto = require("crypto");
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 5000;
 
 const httpServer = createServer();
 
